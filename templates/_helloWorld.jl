f0d0b014b4bfc3ccd172c8c0b962d79072b04eba
Marcin Polak <mapoart@gmail.com>
2019-10-31 13:24:32 +0100

    Julia language implementation

---
+++ templates/_helloWorld.jl
@@ -0,0 +1,23 @@
+# Nexss PROGRAMMER 2.0.0 - Julia
+# Default template for JSON Data
+# STDIN
+NexssStdin = readline();
+
+# JSON in Julia installation
+import Pkg; 
+if !in("JSON",keys(Pkg.installed()))
+    # write(stderr,"pkgJSON is not intalled");
+    @warn("pkgJSON is not intalled")
+    Pkg.add("JSON");  
+end
+import JSON;
+# END JSON part
+
+# Modify Data
+parsedJson = JSON.parse(NexssStdin);
+parsedJson["juliaOutput"] = "Hello from Julia! $VERSION" ;
+#parsedJson["test"] = "test";
+NexssStdout = JSON.json(parsedJson)
+
+# STDOUT
+write(stdout,NexssStdout); 
