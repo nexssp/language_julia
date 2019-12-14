a6eb049036958b55d9b2975ef48442991eb3f497
Marcin Polak <mapoart@gmail.com>
2019-12-14 14:36:50 +0100

    Added readme file and removed old data from Nexss P 1.0

---
+++ README.md
@@ -0,0 +1,8 @@
+# Julia implementation for Nexss Programmer 2.0
+
+WIP: Julia developers please add your content here.
+
+## Useful Links
+
+[julialang.org](https://julialang.org)  
+[Julia Comerial Services - Julia Computing](https://juliacomputing.com/)
--- julia.win32.nexss.config.js
+++ julia.win32.nexss.config.js
@@ -18,19 +18,18 @@ languageConfig.errors = require("./nexss.julia.errors");
 languageConfig.languagePackageManagers = {
   juliapm: {
     // TODO:
-    installation: "PowerShell.exe -File installComposer.ps1",
-    messageAfterInstallation:
-      "Add to the top of your php file(s): require __DIR__ . '/vendor/autoload.php';", //this message will be displayed after this package manager installation, maybe some action needed etc.
-    installed: "composer installed <args>",
-    search: "composer search <args>",
-    install: "composer require <args>",
-    uninstall: "composer remove <args>",
-    help: "composer <args>",
-    version: "composer version",
+    installation: "PowerShell.exe -File juliaPackageManager.ps1",
+    messageAfterInstallation: "",
+    installed: "",
+    search: "juliapm search",
+    install: "juliapm require",
+    uninstall: "juliapm remove <args>",
+    help: "juliapm",
+    version: "juliapm version",
     init: () => {},
     // if command not found in specification
     // run directly on package manager
-    else: "composer <default> <args>"
+    else: "juliapm"
   }
 };
 
