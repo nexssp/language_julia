2a341bbb3fec2faa173b5b20219bdb029f72c867
Marcin Polak <mapoart@gmail.com>
2020-09-03 20:23:04 +0200

    Added Linux and MacOS

--- install/installJulia.sh
+++ install/installJulia.sh
@@ -1,10 +1,11 @@
-git clone -b master git://github.com/JuliaLang/julia.git
+git clone -b master --depth=1 git://github.com/JuliaLang/julia.git ~/.nexssApps/Julia/
 
+cd ~/.nexssApps/Julia/julia
 apt update
 apt install gcc g++ gfortran make
 
-cd julia
 make
 
 export PATH="$(pwd):$PATH"
+echo export PATH="$(pwd):$PATH" > ~/.bashrc
 julia
