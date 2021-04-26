9eff0bfe72caba6ddfddad23669d25cff9ef64ee
Marcin Polak <mapoart@gmail.com>
2021-04-26 11:11:09 +0200

    Removed @nexssp/os dep #36 + Julia Upgrade 1.5.1->1.6.1

--- julia.linux.nexss.config.js
+++ julia.linux.nexss.config.js
@@ -1,14 +1,13 @@
 let languageConfig = Object.assign({}, require("./julia.win32.nexss.config"));
 const installJulia = `${__dirname}/install/installJulia.sh`;
 
-const os = require("@nexssp/os");
-let sudo = os.sudo();
+let sudo = process.sudo;
 
 languageConfig.compilers = {
   julia: {
-    install: ` if [ ! -f ${process.env.NEXSS_APPS_PATH}/julia-1.5.1-linux-x86_64.tar.gz ];then wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.1-linux-x86_64.tar.gz -P ${process.env.NEXSS_APPS_PATH}/; fi
+    install: ` if [ ! -f ${process.env.NEXSS_APPS_PATH}/julia-1.6.1-linux-x86_64.tar.gz ];then wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.1-linux-x86_64.tar.gz -P ${process.env.NEXSS_APPS_PATH}/; fi
 if [ ! -d "${process.env.NEXSS_APPS_PATH}/julia" ]; then mkdir ${process.env.NEXSS_APPS_PATH}/julia ; fi
-tar -xzf ${process.env.NEXSS_APPS_PATH}/julia-1.5.1-linux-x86_64.tar.gz --strip-components 1 -C ${process.env.NEXSS_APPS_PATH}/julia
+tar -xzf ${process.env.NEXSS_APPS_PATH}/julia-1.6.1-linux-x86_64.tar.gz --strip-components 1 -C ${process.env.NEXSS_APPS_PATH}/julia
 cd ${process.env.NEXSS_APPS_PATH}/julia
 ${sudo}ln -s ${process.env.NEXSS_APPS_PATH}/julia/bin/julia /usr/bin/julia`,
     command: "julia",
@@ -27,23 +26,23 @@ ${sudo}make install`,
   },
 };
 
-const distName = os.name();
+const distName = process.distro;
 languageConfig.dist = distName;
 
 // TODO: Later to cleanup this config file !!
 switch (distName) {
-  case os.distros.ARCH:
-    languageConfig.compilers.julia.install = os.replacePMByDistro(
+  case process.distros.ARCH:
+    languageConfig.compilers.julia.install = process.replacePMByDistro(
       `${sudo}pacman -S --noconfirm julia`
     );
     break;
-  case os.distros.UBUNTU:
-    languageConfig.compilers.julia.install = os.replacePMByDistro(
-      `${sudo}apt-get update && ${sudo}apt-get install julia -y`
-    );
-    break;
+  // case process.distros.UBUNTU:
+  //   languageConfig.compilers.julia.install = process.replacePMByDistro(
+  //     `${sudo}apt-get update && ${sudo}apt-get install julia -y`
+  //   );
+  //   break;
   default:
-    languageConfig.compilers.julia.install = os.replacePMByDistro(
+    languageConfig.compilers.julia.install = process.replacePMByDistro(
       languageConfig.compilers.julia.install
     );
     break;
